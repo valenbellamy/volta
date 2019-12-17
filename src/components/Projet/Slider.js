28dcdeb50c95dd5414f47a4a0efa13ba5329c81f
Valentin Bellamy <bellamyvalentin@gmail.com>
2019-12-17 18:46:36 +0100

    speed 4

--- src/components/Projet/Slider.js
+++ src/components/Projet/Slider.js
@@ -125,7 +125,7 @@ const Slider = ({ photos }) => {
     ) {
       var touch = e.changedTouches[0]
       x = touch.pageX
-      speed = 10
+      speed = 4
     } else if (
       e.type === "mousedown" ||
       e.type === "mouseup" ||
