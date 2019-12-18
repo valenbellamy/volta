9f7a06b7b03312139f36f70a839f2d1916298e9e
Valentin Bellamy <bellamyvalentin@gmail.com>
2019-12-18 10:31:04 +0100

    trtdrd

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from "react"
+import React, { useLayoutEffect } from "react"
 import PropTypes from "prop-types"
 
 import "./reset.css"
@@ -13,6 +6,18 @@ import "../fonts/fonts.css"
 import "../styles/index.scss"
 
 const Layout = ({ children }) => {
+  useLayoutEffect(() => {
+    setHeight()
+    window.addEventListener("resize", setHeight)
+    return () => window.removeEventListener("resize", setHeight)
+  }, [])
+
+  const setHeight = () => {
+    let vh = window.innerHeight * 0.01
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`)
+  }
+
   return <>{children}</>
 }
 
