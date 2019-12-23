cbb5a8a7207d5f1f302110805c14ac852c7bdc08
Valentin Bellamy <bellamyvalentin@gmail.com>
2019-12-23 18:21:22 +0100

    title

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Volta Architecture`,
+        short_name: `Volta`,
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#000000`,
--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title }) {
         lang,
       }}
       title={title}
-      titleTemplate={`${site.siteMetadata.title} | Architecture et architecture d'intérieur`}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
           name: `description`,
