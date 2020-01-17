efa5587958b6937c2ea4171c63aa12bdd3709aa5
Valentin Bellamy <bellamyvalentin@gmail.com>
2020-01-17 11:30:22 +0100

    plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,7 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
